59f14121e7e6da3dc3d67d46e9408451dc00c4a8
Levill1 <2336346737@qq.com>
2025-03-04 15:13:14 +0800

    feat: 第7节：工程重构，增加可维护性

--- openai-code-review-sdk/src/main/java/cn/Levionyx/middleware/sdk/infrastructure/git/GitCommand.java
+++ openai-code-review-sdk/src/main/java/cn/Levionyx/middleware/sdk/infrastructure/git/GitCommand.java
@@ -67,7 +67,6 @@ public class GitCommand {
     }
 
     public String commitAndPush(String recommend) throws Exception {
-        logger.info("githubReviewLogUri:{}", githubReviewLogUri);
         Git git = Git.cloneRepository()
                 .setURI(githubReviewLogUri + ".git")
                 .setDirectory(new File("repo"))
