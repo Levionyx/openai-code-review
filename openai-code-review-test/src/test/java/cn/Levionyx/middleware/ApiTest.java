27bdbb9c677abfe2540c3cc63d3cf5cbf82509ba
Levill1 <2336346737@qq.com>
2025-03-04 15:08:36 +0800

    feat: 第7节：工程重构，增加可维护性

--- openai-code-review-test/src/test/java/cn/Levionyx/middleware/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/Levionyx/middleware/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("1234aaaa"));
+        System.out.println(Integer.parseInt("1234aaa1a"));
 
     }
 }
